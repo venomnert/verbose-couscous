8f1c4cb4ff0e3466c283b7032eefb2db1ffd9183
venomnert <neerthigan@gmail.com>
2017-05-30 17:14:57 -0400

    Boilerplate stuffs

---
+++ dev/scripts/main.js
@@ -0,0 +1,15 @@
+const ohHey = "Hello World";
+let test = 'testing';
+let hello = "hello";
+
+console.log(ohHey);
+
+let el = document.querySelector('h1');
+el.addEventListener('click', function() {
+	if (this.innerHTML === 'changed') {
+		this.innerHTML = 'Hello Neerthigan';
+	}
+	else {
+		this.innerHTML = 'changed';
+	}
+})
