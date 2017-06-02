ea4a0257dcb4a141ac4306404592df4637e077ee
venomnert <neerthigan@gmail.com>
2017-06-01 21:51:59 -0400

    finished implementing tinder functionality with retrieved data

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,30 +1,35 @@
 const foodApp = {}
 
+foodApp.userFoodType = "";
+foodApp.userTimeChoiceInSec = 0;
+foodApp.globalRequestCount = 0;
+foodApp.baseUrl = "http://api.yummly.com/v1/api/recipes";
+foodApp.id = '34cb1a7b';
+foodApp.key = 'c6a456b06c87490207e4863b23095a4a';
+foodApp.foodTypes = ['pasta', 'sushi', 'stir-fry'];
+foodApp.likedRecipes = [];
+
 foodApp.init = function () {
  // foodApp.generateCard();
   foodApp.generateHomePage();
   foodApp.homePageEvents();
-
 }
-
-foodApp.baseUrl = "http://api.yummly.com/v1/api/recipes"
-foodApp.id = '34cb1a7b'
-foodApp.key = 'c6a456b06c87490207e4863b23095a4a'
-
-
-foodApp.foodTypes = ['pasta', 'sushi', 'stir-fry']
 
 foodApp.minutesToSeconds = (num) => {
   return num * 60;
 }
-
-
 foodApp.generateHomePage = function() {
+	// Reset globalRequestCount every single homePage is re-rendered;
+	this.globalRequestCount = 0;
+
+	// Remove any previous content on the screen
+	$('.container').empty();
+
   let $homePage = $('<div>').attr('id','homePage');
   let $homePageForm = $('<form>');
-              
 
-  //FOOD TYPE FIELDSET 
+
+  //FOOD TYPE FIELDSET
   let $foodTypeFieldset = $('<fieldset>')
                           .attr('class','foodType');
 
@@ -39,7 +44,7 @@ foodApp.generateHomePage = function() {
     let $foodTypeOption = $('<option>')
                           .attr('value', `${foodApp.foodTypes[i]}`)
                           .text(`${foodApp.foodTypes[i]}`);
-    $foodTypeSelect.append($foodTypeOption); 
+    $foodTypeSelect.append($foodTypeOption);
   }
   let $generatorTitle = $('<h2>')
                         .text('Generator');
@@ -50,7 +55,7 @@ foodApp.generateHomePage = function() {
   // MAX TIME FIELDSET -- WILL INCLUDE TIMER PIC SOON
   let $maxTimeFieldset = $('<fieldset>')
                           .attr('class','maxTime');
-  let $maxTimeDesc = $('<p>').text('How much time do you have?');                  
+  let $maxTimeDesc = $('<p>').text('How much time do you have?');
   //end --- maxTimeFieldset
 
   // radio buttons for maxTime
@@ -58,7 +63,7 @@ foodApp.generateHomePage = function() {
   let $timePic = $('<img>').attr('src', 'assets/timerBG.png');
   let $timeHandle = $('<img>').attr({
                     'src': 'assets/timerHandle.png',
-                    'id': 'handle'}); 
+                    'id': 'handle'});
 
   $timeContainer.append($timePic, $timeHandle);
   for (let i = 1; i <=4; i++) {
@@ -78,7 +83,9 @@ foodApp.generateHomePage = function() {
 
   $homePageForm.append($foodTypeFieldset, $maxTimeFieldset, $submitButton);
   $homePage.append($homePageForm);
-  $('body').append($homePage);
+
+	// Populate container with homepage
+  $('.container').append($homePage);
 }
 
 // EVENTS ON HOMEPAGE EVENTS
@@ -86,42 +93,47 @@ foodApp.homePageEvents = function (){
   $('#submit').on('click', (e) => {
     // prevent defaulting from refresh
     e.preventDefault();
-
-    let foodTypeChoice = $("#foodType").val(); // user food type choice
+    this.userFoodType = $("#foodType").val(); // user food type choice
     let maxTime = parseInt($("input[name=maxTime]:checked").val()); //int value of minutes
 
     //convert maxTime into seconds for query search
-    maxTime = foodApp.minutesToSeconds(maxTime); 
-    console.log(foodTypeChoice, maxTime);
+    this.userTimeChoiceInSec = this.minutesToSeconds(maxTime);
+    console.log(this.userFoodType, this.userTimeChoiceInSec);
 
-    foodApp.getRecipe(foodTypeChoice, maxTime);
+		// Remove home page and make room for overlay
+		$('.container').empty();
+		// Make request and populate container with overlay content
+
+    foodApp.getRecipe(this.userFoodType, this.userTimeChoiceInSec, this.globalRequestCount);
   });
 }
 
-foodApp.getRecipe = function(foodType, maxTime) {
+foodApp.getRecipe = function(foodType, maxTime, startFrom) {
 	var getRecipe = $.ajax({
 		url: foodApp.baseUrl,
 		method: 'GET',
 		dataType: 'jsonp',
 		data: {
-			'_app_id': foodApp.id,   
+			'_app_id': foodApp.id,
 			'_app_key': foodApp.key,
 			format: 'jsonp',
 			requirePictures: true,
 			q: foodType,
       maxTotalTimeInSeconds: maxTime,
-			maxResult: 100,
+			maxResult: 3,
+			start: startFrom,
 		}
 	})
 	.then(function (data){
-		foodApp.generateCard(foodApp.shuffle(data)[0]);
+		let shuffledRecipes = foodApp.shuffle(data);
+		foodApp.generateRecipeList(shuffledRecipes);
 	});
 }
 
 foodApp.shuffle = function(data) {
 	var indexArray = [];
 	var shuffledRecipes = [];
-	console.log("before shuffle", data.matches);
+	// console.log("before shuffle", data.matches);
 	for (let i = 0; i < data.matches.length; i++) {
 		indexArray[i] = i;
 	}
@@ -129,7 +141,7 @@ foodApp.shuffle = function(data) {
 	for (let i = 0; i < indexArray.length; i++) {
 		shuffledRecipes.push(data.matches[indexArray[i]]);
 	}
-	console.log("shuffled", shuffledRecipes);
+	// console.log("shuffled", shuffledRecipes);
 	return shuffledRecipes;
 }
 // Includes the min and excludes the max
@@ -151,17 +163,35 @@ foodApp.shuffleArrayNum = function(array) {
   	}
     return a;
 }
-foodApp.generateRecipeList = function() {
+foodApp.generateRecipeList = function(recipeList) {
+	// Remove any previous decks
+	$('.container').empty();
+
 	let $view = $('<div>')
 							.attr('class', 'deck');
-	$view.append(foodApp.generateCard());
-	$view.on('click', '.recipeCard__newRecipe-btn, .recipeCard__like-btn', function(){
+	let populator = foodApp.recipeCardPopulator(recipeList);
+
+	// console.log(foodApp.generateCard(populator()));
+	$view.append(foodApp.generateCard(populator()));
+
+	$view.on('click', '.recipeCard__newRecipe-btn, .recipeCard__like-btn', function(e){
 		if ($(this).attr('class') === 'recipeCard__like-btn') {
-	    console.log('like clicked');
+	    console.log('like clicked', e);
 			foodApp.jTinderAdd('swipe-right');
 			$('.recipeCard').on('animationend oAnimationEnd mozAnimationEnd webkitAnimationEnd', function(){
 				console.log('animation done');
+					// Save the liked recipe
+					for (let i = 0; i < recipeList.length; i++) {
+						if (recipeList[i].id === $('.recipeCard').attr('data-id')) {
+							foodApp.likedRecipes.push(recipeList[i]);
+							break;
+						}
+					}
+					// Remove the recipe after animation is done
 			    $('.deck').empty();
+
+					// Get a new recpie  and add to the cart
+					$('.deck').append(foodApp.generateCard(populator()));
 			});
 		}
 		else {
@@ -170,6 +200,7 @@ foodApp.generateRecipeList = function() {
 			$('.recipeCard').on('animationend oAnimationEnd mozAnimationEnd webkitAnimationEnd', function(){
 				console.log('animation done');
 					$('.deck').empty();
+					$('.deck').append(foodApp.generateCard(populator()));
 			});
 		}
 	});
@@ -178,20 +209,28 @@ foodApp.generateRecipeList = function() {
 }
 foodApp.recipeCardPopulator = function(data) {
 	let index = -1;
-
 	return function() {
 		index++;
 		if (index < data.length) {
 			return data[index];
 		}
 		else {
+			// Make ajax request
+			console.log('make ajax request');
+			// Increment globalRequestCount by index
+			foodApp.globalRequestCount += index;
 
+			// When we make a new ajax request, we are returning no data
+			// As a result we will see a temporary error on our console regarding undefined data
+			// What is a safe way to fail?
+			foodApp.getRecipe(foodApp.userFoodType, foodApp.userTimeChoiceInSec, foodApp.globalRequestCount);
 		}
 	}
 }
 foodApp.jTinderAdd = function add(name){
 		$('.recipeCard').addClass(name);
 }
+
 // Create a recipe card dynamically
 foodApp.generateCard = function(data) {
   // See https://stackoverflow.com/questions/22075730/css-background-image-url-path
@@ -201,9 +240,10 @@ foodApp.generateCard = function(data) {
 
   let $card = $('<div>')
               .attr({
-                'class': 'recipeCard'
-              })
-              .css({'background': `url(${fixedImage})`, 'background-size': 'cover'})
+								'class':'recipeCard',
+								'data-id': data.id
+							})
+              .css({'background': `url(${fixedImage})`, 'background-size': 'cover'});
               console.log(data)
   let $backSection = $('<div>')
                     .attr('class', 'backSection');
@@ -217,10 +257,11 @@ foodApp.generateCard = function(data) {
   $backButton.append($backButtonImg);
   $backButton.on('click', function() {
     console.log('go back to home page');
+		foodApp.generateHomePage();
   })
   let $backTitle = $('<p>')
                     .attr('class', 'backSection__title')
-                    .text('Stir-fry');
+                    .text(foodApp.userFoodType);
                     // .text(); retrieve the value from the select
   $backSection.append($backButton, $backTitle);
 
@@ -245,7 +286,7 @@ foodApp.generateCard = function(data) {
 		                    data.ingredients.forEach(function(data) {
                         	$ingredientItem.append('<li>' + data + '</li>')
                         	$ingredientList.append($ingredientItem)
-                        	console.log(data)
+                        	// console.log(data)
                         })
 
 	let $newRecipeBtn = $('<button>')
@@ -256,10 +297,9 @@ foodApp.generateCard = function(data) {
   $card.append($backSection, $likeBtn, $foodTitle, $ingredientSection, $newRecipeBtn);
   return $card;
 }
-
- foodApp.imgSizeChange = function(data) {
- 	console.log(data);
-  console.log('isndie trimmer', data.substr(0, data.length-4));
+foodApp.imgSizeChange = function(data) {
+ // 	console.log(data);
+  // console.log('isndie trimmer', data.substr(0, data.length-4));
   return data.substr(0, data.length-4);
 	// $(generateCard.smallImageUrls[0]).toString('=s90', '')
 	// console.log(generateCard.smallImageUrls[0])
@@ -274,7 +314,9 @@ foodApp.likeButton = function(text) {
     </a>`;
 }
 
-
+foodApp.generateGrid = function() {
+	
+}
 
 $(function(){
 	foodApp.init();
